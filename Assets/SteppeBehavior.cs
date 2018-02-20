5148c896b70907c4cbdc76d7fc4f79d4864f9be5
dschwarz-think\David Schwarz <dschwarz@lenovo.com>
2018-02-20 10:53:13 -0500

    Initial commit for building Hololens apps for gait rehab.

---
+++ Assets/SteppeBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteppeBehavior : MonoBehaviour {
+
+    public Material OffMaterial;
+    public Material OnMaterial;
+
+    public float blendDuration = 1.0f;
+    // Use this for initialization
+    bool steppedOn = false;
+
+    void Start()
+    {
+    }
+    void Update()
+    {
+        //float lerp = Mathf.PingPong(Time.time, blendDuration) / blendDuration;
+        //rend.material.Lerp(OffMaterial, OnMaterial, lerp);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!steppedOn)
+            StartCoroutine(TurnOnAnimation(GetComponent<Renderer>()));
+
+        steppedOn = true;
+    }
+
+    IEnumerator TurnOnAnimation(Renderer rend)
+    {
+        
+        float elapsedTime = 0;
+
+        while (elapsedTime < blendDuration)
+        {
+            float lerp = elapsedTime / blendDuration;
+            rend.material.Lerp(OffMaterial, OnMaterial, lerp);
+            elapsedTime += Time.deltaTime;
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
---
+++ Assets/_MK/MKGlowFree/Example/MKGlowDemoFreeControl.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MK.Glow;
+
+public class MKGlowDemoFreeControl : MonoBehaviour
+{
+    private bool showMenu = false;
+    [SerializeField]
+    private MKGlowFree mkGlow;
+    [SerializeField]
+    private UnityStandardAssets.Characters.FirstPerson.FirstPersonController controller;
+    [SerializeField]
+    private GameObject ui;
+
+    [SerializeField]
+    private GameObject uiStartText;
+
+    public float GlowIntensity
+    {
+        set
+        {
+            mkGlow.GlowIntensity = value;
+        }
+    }
+    public float BlurSpread
+    {
+        set
+        {
+            mkGlow.BlurSpread = value;
+        }
+    }
+    public float BlurIterations
+    {
+        set
+        {
+            mkGlow.BlurIterations = (int)value;
+        }
+    }
+
+    private void Start ()
+    {
+        ui.SetActive(false);
+    }
+	
+	private void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.M))
+        {
+            showMenu = !showMenu;
+
+            if(showMenu)
+            {
+                controller.enabled = false;
+                ui.SetActive(true);
+                if (uiStartText)
+                    Destroy(uiStartText);
+            }
+            else
+            {
+                controller.enabled = true;
+                ui.SetActive(false);
+            }
+        }
+	}
+}
